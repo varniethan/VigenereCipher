6a09a1c362b1ed5458f09dde6794e083c85c31d3
varniethan <varniethan@protonmail.com>
2023-03-17 15:30:04 +0000

    Started the linked list - half of the functions done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Vigenere Cipher World");
+        VigenereCipher cipher = new VigenereCipher();
+        VigenereCipher_2 cipher_42 = new VigenereCipher_2();
+//        cipher.readfile("./src/encrypt_check.txt");
+//        cipher_42.encrypt("encrypt_check.txt", "key_check.txt");
+//        cipher_42.decrypt("decrypt_check.txt", "key_check.txt");
+        ArrayList<Double> test_vals = new ArrayList<Double>();
+        //9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0
+        test_vals.add(9.0);test_vals.add(8.0);test_vals.add(7.0);test_vals.add(6.0);test_vals.add(5.0);test_vals.add(4.0);test_vals.add(3.0);test_vals.add(2.0);test_vals.add(1.0);
+        System.out.println("Original values: " + test_vals.toString());
+        CustomSort ss = new CustomSort();
+        ss.setValues(test_vals);
+        System.out.println("Sorted values: " + test_vals.toString());
+        System.out.println("Gaps used: " + ss.getGaps().toString());
     }
 }
--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -18,6 +21,7 @@ public class VigenereCipher implements Cipher
             while (reader.hasNextLine())
             {
                 data = reader.nextLine();
+//                data += "\n";
                 data_String.add(data);
             }
             reader.close();
@@ -26,6 +30,23 @@ public class VigenereCipher implements Cipher
             System.out.println("An error occurred");
             e.printStackTrace();
         }
+         var filePath = Paths.get("./src/"+filename);
+
+        try{
+
+            String content = Files.readString(filePath);
+            System.out.println("String Present:"+content);
+            content = content.toUpperCase();
+            for (int i = 0; i < content.length(); i++)
+            {
+                System.out.print(content.charAt(i));
+            }
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+//        System.out.println(data);
         return data_String;
     }
     @Override
@@ -34,15 +55,13 @@ public class VigenereCipher implements Cipher
         //Split the message
         ArrayList<String> message_string = this.readfile(message_filename);
         ArrayList<String> key_string = this.readfile(key_filename);
-        int key_size = key_string.size();
-        String key = (this.readfile(key_filename)).toString();
         StringBuilder builder = new StringBuilder(key_string.size());
         for(String ch: key_string)
         {
             builder.append(ch);
         }
-        builder.toString();
         int key_length = builder.length();
+        String key = builder.toString();
         ArrayList<ArrayList> splited_line = new ArrayList<>();
         for (String message: message_string)
         {
@@ -70,13 +89,50 @@ public class VigenereCipher implements Cipher
             splited_line.add(splited_word);
         }
         System.out.println(splited_line);
-
+        String result = "";
+//        StringBuilder result = new StringBuilder(key_string.size());
+        int j = 0;
         for (ArrayList line: splited_line)
         {
             for (Object word: line)
             {
+//                System.out.println(word);
+//                System.out.println(word.toString().length());
+                String word_to_decrypt = word.toString();
+                for (int i = 0; i < word_to_decrypt.length(); i++)
+                {
+                    char message_letter = word_to_decrypt.charAt(i);
+                    System.out.println("Mletter: "+message_letter);
+                    System.out.println("J: "+j);
+                    if (i == 0 && Character.isWhitespace(message_letter))
+                        continue;
+                    if (Character.isLetter(message_letter))
+                    {
+                        char key_letter = key.charAt(j);
+                        int message_letter_index = (int) message_letter - 65;
+                        int key_letter_index = (int) key_letter - 65;
+                        System.out.println("M: "+message_letter+" : "+message_letter_index+" K: "+key_letter+" : "+key_letter_index);
+                        int number = (message_letter_index + key_letter_index) % 26;
+                        System.out.println("EXP :"+(message_letter_index + key_letter_index));
+                        System.out.println(number);
+                        System.out.println("Encrypted: "+(char) (number+65));
+                        result += ((char) (number+65));
+                    }
+                    else
+                    {
+                        result += message_letter;
+                    }
+                    j = (j+1) % key_length;
 
+//                    j++;
+//                    if (j >= key_length-1)
+//                    {
+//                        j = 0;
+//                    }
+                }
+                System.out.println(result);
             }
+            j++;
         }
         return null;
     }
